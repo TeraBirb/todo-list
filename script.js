59fae6cc3cfde2cc03a5665d77970f91fb0b82a4
Isaiah <isaiah.g.callano@gmail.com>
2019-12-09 01:21:20 -0800

    add value check on inputs, add alert animation, set default popup showing

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const popupTitle = document.querySelector(".popup input");
 const popupTextarea = document.querySelector(".popup textarea");
 const popupAdd = document.querySelector(".popup button");
 const todoContainer = document.querySelector(".todo-container");
+const alert = document.querySelector(".alert");
 
 function deleteNote(e) {
   e.target.parentNode.parentNode.style.display = "none";
@@ -19,33 +20,45 @@ popupClose.addEventListener("click", () => {
 });
 
 popupAdd.addEventListener("click", e => {
-  let todo = document.createElement("div");
-  let todoHeader = document.createElement("div");
-  let h2 = document.createElement("h2");
-  let span = document.createElement("span");
-  let p = document.createElement("p");
+  if (popupTitle.value === "" && popupTextarea.value === "") {
+    popupTitle.placeholder = "Try typing something here";
+    popupTextarea.placeholder = "...or here";
+  } else {
+    let todo = document.createElement("div");
+    let todoHeader = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let span = document.createElement("span");
+    let p = document.createElement("p");
 
-  todo.classList = "todo";
-  todoHeader.classList = "todo-header";
-  h2.classList = "todo-title";
-  p.classList = "todo-body";
+    todo.classList = "todo";
+    todoHeader.classList = "todo-header";
+    h2.classList = "todo-title";
+    p.classList = "todo-body";
 
-  h2.innerHTML = popupTitle.value;
-  span.textContent = "x";
-  span.addEventListener("click", deleteNote);
+    h2.innerHTML = popupTitle.value;
+    span.textContent = "x";
+    span.addEventListener("click", deleteNote);
 
-  todoHeader.appendChild(h2);
-  todoHeader.appendChild(span);
-  p.append(popupTextarea.value);
+    todoHeader.appendChild(h2);
+    todoHeader.appendChild(span);
+    p.append(popupTextarea.value);
 
-  todo.appendChild(todoHeader);
-  todo.appendChild(p);
+    todo.appendChild(todoHeader);
+    todo.appendChild(p);
 
-  todoContainer.appendChild(todo);
+    todoContainer.appendChild(todo);
 
-  console.log(todo);
+    console.log(todo);
 
-  e.target.parentNode.classList = "popup hidden";
-  popupTitle.value = "";
-  popupTextarea.value = "";
+    e.target.parentNode.classList = "popup hidden";
+    popupTitle.value = "";
+    popupTextarea.value = "";
+    popupTitle.placeholder = "Title";
+    popupTextarea.placeholder = "Add a note";
+
+    alert.classList = "alert";
+    setTimeout(function() {
+      alert.classList = "alert hidden";
+    }, 1900);
+  }
 });
